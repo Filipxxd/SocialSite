e1b21d31dce85386da88c7952df336db9016c765
Filip Chromečka <filip.chromecka@outlook.com>
2024-10-11 22:53:13 +0200

    GetDateOnly return current

--- SocialSite.Core/Utilities/DateTimeProvider.cs
+++ SocialSite.Core/Utilities/DateTimeProvider.cs
@@ -5,5 +5,5 @@ namespace SocialSite.Core.Utilities;
 public sealed class DateTimeProvider : IDateTimeProvider
 {
     public DateTime GetDateTime() => DateTime.Now;
-    public DateOnly GetDateOnly() => new();
+    public DateOnly GetDateOnly() => DateOnly.FromDateTime(DateTime.Now);
 }
