54dcf7de8d1bed83fa2e2cbc8ca4b8cba84b1086
Filip Chromečka <filip.chromecka@outlook.com>
2024-10-12 19:02:57 +0200

    Remove Web Project

--- SocialSite.Web/Routes.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SocialSite.Web;
-
-public static class Routes
-{
-    public const string Home = "/";
-    public const string Login = "/login";
-    public const string Logout = "/logout";
-}
