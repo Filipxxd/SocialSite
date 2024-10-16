f919dd5dff75dc33ae37f461a81bb1d8bd73f636
f_chromecka <filip.chromecka@outlook.com>
2024-10-16 16:54:39 +0200

    Base Implementation

--- SocialSite.Application/AppServices/AccountAppService.cs
+++ SocialSite.Application/AppServices/AccountAppService.cs
@@ -6,6 +6,7 @@ using SocialSite.Application.Dtos.Account;
 using SocialSite.Core.Constants;
 using SocialSite.Core.Utilities;
 using SocialSite.Domain.Models;
+using SocialSite.Domain.Services;
 using SocialSite.Domain.Utilities;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -15,52 +16,36 @@ namespace SocialSite.Application.AppServices;
 
 public sealed class AccountAppService
 {
-    private readonly UserManager<User> _userManager;
+    private readonly IAccountService _accountService;
     private readonly IOptions<JwtSetup> _options;
 
-    public AccountAppService(UserManager<User> userManager, IOptions<JwtSetup> options)
+    public AccountAppService(UserManager<User> userManager, IOptions<JwtSetup> options, IAccountService accountService)
     {
-        _userManager = userManager;
         _options = options;
+        _accountService = accountService;
     }
 
     public async Task<Result> RegisterAsync(RegisterDto dto)
     {
-        var userExists = await _userManager.FindByNameAsync(dto.UserName);
-        if (userExists != null)
-            return Result.Fail(ResultErrors.NotValid, $"User with given username: '{dto.UserName}' already exists.");
-
         var user = dto.Adapt<User>();
 
-        var result = await _userManager.CreateAsync(user, dto.Password);
-        if (!result.Succeeded)
-            return Result.Fail(ResultErrors.NotValid, result.Errors.Select(e => e.Description));
-
-        return Result.Success();
+        return await _accountService.RegisterAsync(user, dto.Password);
     }
 
     public async Task<Result<TokenDto>> LoginAsync(LoginDto dto)
     {
-        var user = await _userManager.FindByNameAsync(dto.UserName);
-        if (user is null)
-            return Result<TokenDto>.Fail(ResultErrors.NotValid, "Invalid credentials.");
+        var result = await _accountService.LoginAsync(dto.UserName, dto.Password);
 
-        var passwordValid = await _userManager.CheckPasswordAsync(user, dto.Password);
-        if (!passwordValid)
-            return Result<TokenDto>.Fail(ResultErrors.NotValid, "Invalid credentials.");
-
-        var userRoles = await _userManager.GetRolesAsync(user);
+        if (!result.IsSuccess)
+            return Result<TokenDto>.Fail(ResultErrors.NotValid, result.Errors.SelectMany(e => e.Value));
 
         var authClaims = new List<Claim>
         {
-            new(ClaimTypes.Name, user.UserName),
+            new(ClaimTypes.Name, dto.UserName),
             new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
         };
 
-        foreach (var userRole in userRoles)
-        {
-            authClaims.Add(new Claim(ClaimTypes.Role, userRole));
-        }
+        authClaims.AddRange(result.Entity);
 
         var token = GetToken(authClaims);
 
---
+++ SocialSite.Domain/Services/IAccountService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SocialSite.Domain.Models;
+using SocialSite.Domain.Utilities;
+using System.Security.Claims;
+
+namespace SocialSite.Domain.Services;
+
+public interface IAccountService
+{
+    Task<User> GetUserByIdAsync(int currentUserId);
+    Task<Result> RegisterAsync(User user, string password);
+    Task<Result<IEnumerable<Claim>>> LoginAsync(string userName, string password);
+}
