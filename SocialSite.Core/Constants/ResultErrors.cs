2e41f21ce29c265351340b15bd5f32bfece620d8
Filip Chromečka <filip.chromecka@outlook.com>
2024-10-29 21:11:45 +0100

    Remove Dead Code

--- SocialSite.Core/Constants/ResultErrors.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SocialSite.Core.Constants;
-
-public static class ResultErrors
-{
-    public const string NotFound = nameof(NotFound);
-    public const string NotValid = nameof(NotValid);
-    public const string NotAuthorized = nameof(NotAuthorized);
-    public const string Concurrency = nameof(Concurrency);
-}
