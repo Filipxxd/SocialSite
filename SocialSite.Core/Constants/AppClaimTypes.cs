41f89556c233b60e6148ef06dd45c833790041ff
Filip Chromečka <filip.chromecka@outlook.com>
2024-11-23 15:08:50 +0100

    FE Friendly ClaimTypes

--- SocialSite.Core/Constants/AppClaimTypes.cs
+++ SocialSite.Core/Constants/AppClaimTypes.cs
@@ -2,6 +2,6 @@ namespace SocialSite.Core.Constants;
 
 public static class AppClaimTypes
 {
-    public const string UserId = nameof(UserId);
-    public const string Fullname = nameof(Fullname);
+    public const string UserId = "userId";
+    public const string Fullname = "fullname";
 }
