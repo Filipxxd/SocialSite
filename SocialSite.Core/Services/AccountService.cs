1e4d2cc4823cfdfecf043072f541c4dab6dc3f4d
Filip Chromečka <filip.chromecka@outlook.com>
2024-12-28 20:03:16 +0100

    Add user to default role

--- SocialSite.Core/Services/AccountService.cs
+++ SocialSite.Core/Services/AccountService.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using SocialSite.Core.Constants;
 using SocialSite.Core.Exceptions;
 using SocialSite.Data.EF;
+using SocialSite.Domain.Constants;
 using SocialSite.Domain.Models;
 using SocialSite.Domain.Services;
 
@@ -43,6 +44,8 @@ public sealed class AccountService : IAccountService
 		var result = await _userManager.CreateAsync(user, password);
 		if (!result.Succeeded)
 			throw new NotValidException(string.Join(", ", result.Errors.Select(e => e.Description)));
+
+		await _userManager.AddToRoleAsync(user, Roles.User);
 	}
 	
 	public async Task<IEnumerable<Claim>> GetUserClaimsAsync(int userId)
