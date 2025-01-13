33c891a8eb8bb6bc8657870c7d52fd3dccd2984d
Filip Chromečka <filip.chromecka@outlook.com>
2025-01-13 17:56:01 +0100

    Remove unused prop

--- SocialSite.Data/EF/TestDataSeeder.cs
+++ SocialSite.Data/EF/TestDataSeeder.cs
@@ -11,7 +11,6 @@ public sealed class TestDataSeeder
 
 	private List<Role> Roles { get; set; } = [];
 	private List<User> Users { get; set; } = [];
-	private List<UserRole> AspNetUserRoles { get; set; } = [];
 	private List<FriendRequest> FriendRequests { get; set; } = [];
 	private List<Friendship> Friendships { get; set; } = [];
 	private List<Post> Posts { get; set; } = [];
@@ -70,7 +69,7 @@ public sealed class TestDataSeeder
 
 	private async Task DeleteDataAndResetIdentity()
 	{
-		var tableNames = new string[]
+		var tableNames = new[]
 		{
 			Tables.Reports,
 			Tables.Comments,
