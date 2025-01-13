f633ba45f990ca805ae4721f2aaba5714158054b
Filip Chromečka <filip.chromecka@blogic.cz>
2025-01-13 20:28:52 +0100

    Fix Validation

--- SocialSite.Application/Validators/Account/UpdateProfileDtoValidator.cs
+++ SocialSite.Application/Validators/Account/UpdateProfileDtoValidator.cs
@@ -4,15 +4,14 @@ using SocialSite.Application.Dtos.Users;
 
 namespace SocialSite.Application.Validators.Account;
 
-public sealed class UpdateProfileDtoValidator: AbstractValidator<UpdateProfileDto>
+public sealed class UpdateProfileDtoValidator : AbstractValidator<UpdateProfileDto>
 {
-    public UpdateProfileDtoValidator()
-    {
-        RuleFor(e => e.Firstname).NotEmpty()
-            .Matches(ValidationConstants.CzechAlphabetRegex).WithMessage("'Firstname' must contain only characters from czech alphabet");
-        RuleFor(e => e.Lastname).NotEmpty()
-            .Matches(ValidationConstants.CzechAlphabetRegex).WithMessage("'Lastname' must contain only characters from czech alphabet");
-        RuleFor(e => e.Bio).MaximumLength(500)
-            .Matches(ValidationConstants.CzechAlphabetRegex).WithMessage("'Lastname' must contain only characters from czech alphabet");
-    }
+	public UpdateProfileDtoValidator()
+	{
+		RuleFor(e => e.Firstname).NotEmpty()
+			.Matches(ValidationConstants.CzechAlphabetRegex).WithMessage("'Firstname' must contain only characters from czech alphabet");
+		RuleFor(e => e.Lastname).NotEmpty()
+			.Matches(ValidationConstants.CzechAlphabetRegex).WithMessage("'Lastname' must contain only characters from czech alphabet");
+		RuleFor(e => e.Bio).MaximumLength(500);
+	}
 }
